04ecbd82132051c61a44518f24bfe139d49a134c
TAASCRIA <riccardo.scotti@swisscom.com>
2025-07-31 11:09:08 +0200

    Initial commit

--- lib/aircraft_loader.py
+++ lib/aircraft_loader.py
@@ -10,8 +10,9 @@ class AircraftLoader:
         with open(filepath, 'r') as f:
             return json.load(f)
 
-    def set_speed(self, speed_value: int) -> bool:
+    def set_speed(self, speed_value: int, cpflight_comd:str, sock) -> bool:
         if self.speed["op"]:
             return False
         self.speed["value"] = speed_value
+        sock.sendall((cpflight_comd.format(value=speed_value) + "\n").encode())
         return True
--- lib/loop_controller.py
+++ lib/loop_controller.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import socket
+import select
 from typing import Optional
 
 from lib.aircraft_loader import AircraftLoader
@@ -16,11 +17,14 @@ class LoopController:
         self.get_interval = get_interval_callback
         self.running = False
         self.thread = None
+        self.socket_thread = None
+
         self.aircraft = obj_aircraft
         self.current_config = None
 
         self.sm = None
         self.vr = None
+        self.sock = None
 
     def start(self, config, cpflight) -> tuple[bool, Optional[str]]:
         if self.running:
@@ -36,12 +40,15 @@ class LoopController:
             return False, str(e)
 
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect((self.cpflight.get('IP'), self.cpflight.get('PORT')))
-            sock.setblocking(False)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.cpflight.get('IP'), self.cpflight.get('PORT')))
+            self.sock.setblocking(False)
             self.running = True
             self.thread = threading.Thread(target=self._run_loop, daemon=True)
+            self.socket_thread = threading.Thread(target=self._socket_listener_loop, daemon=True)
+
             self.thread.start()
+            self.socket_thread.start()
             return True, None
         except Exception as e:
             return False, f"CpFlight Connection error: {e}"
@@ -50,10 +57,43 @@ class LoopController:
         self.running = False
         self.sm = None
         self.vr = None
+        if self.sock:
+            try:
+                self.sock.shutdown(socket.SHUT_RDWR)
+                self.sock.close()
+            except Exception:
+                pass
+            self.sock = None
 
     def _run_loop(self):
         while self.running:
-            interval = self.get_interval()
-            #speed
-            self.aircraft.set_speed(int(self.vr.get(self.current_config.get('speed').get("rx"))))
-            time.sleep(interval)
+            try:
+                interval = self.get_interval()
+                # Read from SimConnect and send to hardware
+                self.aircraft.set_speed(
+                    int(self.vr.get(self.current_config.get('speed').get("rx"))),
+                    self.cpflight.get('speed').get('tx'),
+                    self.sock
+                )
+
+                time.sleep(interval)
+            except Exception as e:
+                time.sleep(1)
+
+    def _socket_listener_loop(self):
+        while self.running and self.sock:
+            try:
+                readable, _, _ = select.select([self.sock], [], [], 0.1)
+                if self.sock in readable:
+                    try:
+                        data = self.sock.recv(1024)
+                        if not data:
+                            self.stop()
+                            break
+                        value_from_fcu = data.decode(errors="ignore")
+                    except BlockingIOError:
+                        pass
+                    except Exception:
+                        pass
+            except Exception as e:
+                pass
