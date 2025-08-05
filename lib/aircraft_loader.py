adb284f6d3952fdf84fd1a14ea04353b5aeed305
RickyS. <scottiriccardo@icloud.com>
2025-08-05 21:27:32 +0200

    Initial Commit

--- lib/aircraft_loader.py
+++ lib/aircraft_loader.py
@@ -44,6 +44,10 @@ class AircraftLoader:
         self.speed["nack"] = not bool(vr.get(f"({aircraft_array.get('speed').get('extra_mach')})"))
         self.heading["trk"] = not bool(vr.get(f"({aircraft_array.get('heading').get('extra_trk')})"))
 
+        # QNH Label EFIS
+        sock.sendall((cpflight_cmds.get("qnh_cp").get("tx_label_qnh") + "\n").encode())
+        sock.sendall((cpflight_cmds.get("qnh_fo").get("tx_label_qnh") + "\n").encode())
+
     def set_dot_fcu(self, aircraft_array: int, cpflight_cmds:dict, sock, vr) -> bool:
         speed_var_dot = aircraft_array.get("speed").get('extra_dot')
         heading_var_dot = aircraft_array.get("heading").get('extra_dot')
