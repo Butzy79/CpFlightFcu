3e624277012593224c57f922479547a46715c8bc
Butzy79 <scottiriccardo@icloud.com>
2026-04-10 23:52:12 +0200

    2.1.1

--- lib/aircraft_loader.py
+++ lib/aircraft_loader.py
@@ -742,12 +742,13 @@ class AircraftLoader:
         self.btn_gen["op"] = False
 
     def set_btn_cp_fd_aircraft(self, value: str, config, vr, sock, cpfligh):
-        new_value = not self.led_fo_efis['led_cp_fd']
+        which_led = 'led_cp_fd'
+        new_value = not self.led_cp_efis[which_led]
         for el in config['btn_cp_fd']['tx']:
             actual = vr.get(f"({el})")
             vr.set(f"{int(actual+2)} (>{el})")
-        sock.sendall((cpfligh["led_cp_fd"]["led_on" if new_value else "led_off"] + "\n").encode())
-        self.led_cp_efis["led_cp_fd"] = new_value
+        sock.sendall((cpfligh[which_led]["led_on" if new_value else "led_off"] + "\n").encode())
+        self.led_cp_efis[which_led] = new_value
         self.btn_gen["op"] = False
 
     def set_btn_cp_ls_aircraft(self, value: str, config, vr, sock, cpfligh):
